3e79e38a3fbd8e536b52efd96b24d3e2592cbc55
Reed Branson <reed.branson@gmail.com>
2016-06-27 17:38:33 -0400

    successfully rendering index.html

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var swig = require('swig');
 
 var app = express();
 var routes = require('./routes');
+
+app.engine('html', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Activity = require('../models/activity');
+var Hotel = require('../models/hotel');
+var Restaurant = require('../models/restaurant');
+var Promise = require('bluebird');
+
 
 router.get('/', function(req, res, next){
-  console.log('here');
-  res.send('heloooo')
-
-})
+  Promise.all([Activity.findAll(), Hotel.findAll(), Restaurant.findAll()])
+  .spread(function (activities, hotels, restaurants) {
+    res.render('index', {activities: activities, hotels: hotels, restaurants: restaurants});
+  });
+});
 
 module.exports = router;
